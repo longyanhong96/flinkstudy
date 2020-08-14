2bb3396782ecd4c3f457032d351c9ddbf619c9b2
龙衍鸿 <long.yanhong@pushiai.com>
2020-08-15 02:58:18 +0800

    fix:测流的测试

--- src/main/scala/arithmetic/SideOutDemo.scala
+++ src/main/scala/arithmetic/SideOutDemo.scala
@@ -23,7 +23,7 @@ object SideOutDemo {
         val values = line.split(",")
         StationLog(values(0), values(1), values(2), values(3), values(4).toLong, values(5).toLong)
       })
-      .process(new SideOutProcessFunctions)
+      .process(new SideOutProcessFunctions2)
 
     sideOutStream.print("成功的")
     sideOutStream.getSideOutput(tag).print("不成功的")
@@ -37,6 +37,15 @@ object SideOutDemo {
       if (i.callType.equals("success")) {
         collector.collect(i)
       } else {
+        context.output(tag, i)
+      }
+    }
+  }
+
+  class SideOutProcessFunctions2 extends ProcessFunction[StationLog, StationLog] {
+    override def processElement(i: StationLog, context: ProcessFunction[StationLog, StationLog]#Context, collector: Collector[StationLog]): Unit = {
+      collector.collect(i)
+      if (!i.callType.equals("success")) {
         context.output(tag, i)
       }
     }
